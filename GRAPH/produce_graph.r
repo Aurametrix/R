21671450e311eafd628bf51080ebbf15e65c062b
Irene <irene@aurametrix.com>
2014-07-07 22:04:57 -0700

    add a red line for trucks vetor & specify the y-axis range directly

--- GRAPH/produce_graph.r
+++ GRAPH/produce_graph.r
@@ -1,8 +1,12 @@
-# Define the cars vector with 5 values
+# Define 2 vectors
 cars <- c(1, 3, 6, 4, 9)
+trucks <- c(2, 5, 4, 5, 12)
 
-# Graph the cars vector with all defaults
-plot(cars)
+# Graph cars using a y axis that ranges from 0 to 12
+plot(cars, type="o", col="blue", ylim=c(0,12))
+
+# Graph trucks with red dashed line and square points
+lines(trucks, type="o", pch=22, lty=2, col="red")
 
 # Create a title with a red, bold/italic font
 title(main="Autos", col.main="red", font.main=4)
