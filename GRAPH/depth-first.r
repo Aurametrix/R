acdb26f4e35d85df97744a636e528fbad280d246
Irene <irene@aurametrix.com>
2014-11-24 22:02:27 -0800

    another DFS example

--- GRAPH/depth-first.r
+++ GRAPH/depth-first.r
@@ -10,10 +10,33 @@
     father = FALSE, dist = FALSE, in.callback = NULL,
     out.callback = NULL, extra = NULL, rho = parent.frame())
 
-# example 1
+### example 1
   RNGkind("Mersenne-Twister")
   set.seed(123)
   g1 <- randomGraph(letters[1:10], 1:4, p=.3)
   RNGkind()
   DFS(g1, "a")
 
+### example 2
+## A graph with two separate trees
+graph.dfs(graph.tree(10) %du% graph.tree(10), root=1, "out",
+          TRUE, TRUE, TRUE, TRUE)
+ 
+## How to use a callback
+f.in <- function(graph, data, extra) {
+  cat("in:", paste(collapse=", ", data), "\n")
+  FALSE
+}
+f.out <- function(graph, data, extra) {
+  cat("out:", paste(collapse=", ", data), "\n")
+  FALSE
+}
+tmp <- graph.dfs(graph.tree(10), root=1, "out",
+                 in.callback=f.in, out.callback=f.out)
+ 
+## Terminate after the first component, using a callback
+f.out <- function(graph, data, extra) {
+ data['vid'] == 1
+}
+tmp <- graph.dfs(graph.tree(10) %du% graph.tree(10), root=1,
+                 out.callback=f.out)
