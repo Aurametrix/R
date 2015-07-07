ee73bca078a17f8009db5ca7258af5b1a42117f7
Aurametrix <irene@aurametrix.com>
2015-07-06 17:36:04 -0700

    Kruskal-Wallis Test
    to decide whether the population distributions are identical without assuming them to follow the normal distribution.
    
    The ANOVA (and t-test) is explicitly a test of equality of means of values. The Kruskal-Wallis (and Mann-Whitney) can be seen technically as a comparison of the mean ranks, testing whether the probability that a random observation from each group is equally likely to be above or below a random observation from another group.

---
+++ ML/kruskal.r
@@ -0,0 +1,22 @@
+## Hollander & Wolfe (1973), 116.
+## Mucociliary efficiency from the rate of removal of dust in normal
+##  subjects, subjects with obstructive airway disease, and subjects
+##  with asbestosis.
+x <- c(2.9, 3.0, 2.5, 2.6, 3.2) # normal subjects
+y <- c(3.8, 2.7, 4.0, 2.4)      # with obstructive airway disease
+z <- c(2.8, 3.4, 3.7, 2.2, 2.0) # with asbestosis
+kruskal.test(list(x, y, z))
+## Equivalently,
+x <- c(x, y, z)
+g <- factor(rep(1:3, c(5, 4, 5)),
+            labels = c("Normal subjects",
+                       "Subjects with obstructive airway disease",
+                       "Subjects with asbestosis"))
+kruskal.test(x, g)
+ 
+## Formula interface.
+require(graphics)
+boxplot(Ozone ~ Month, data = airquality)
+kruskal.test(Ozone ~ Month, data = airquality)
+
+## kruskal.test(depVar ~ indepVar, data=df)
