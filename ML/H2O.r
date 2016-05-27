f10fee4a1e3373a71404c472ba37c22eddb763f8
Aurametrix <irene@aurametrix.com>
2016-05-26 18:30:57 -0700

    install the h2o R library
    http://www.h2o.ai/download/h2o/r  
    tutorial with an example: https://github.com/h2oai/h2o-tutorials/tree/master/h2o-open-tour-2016/chicago

---
+++ ML/H2O.r
@@ -0,0 +1,18 @@
+
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download packages that H2O depends on.
+pkgs <- c("methods","statmod","stats","graphics","RCurl","jsonlite","tools","utils")
+for (pkg in pkgs) {
+    if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
+
+# Now we download, install and initialize the H2O package for R.
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turchin/6/R")))
+library(h2o)
+localH2O = h2o.init(nthreads=-1)
+
+# Finally, let's run a demo to see H2O at work.
+demo(h2o.kmeans)
