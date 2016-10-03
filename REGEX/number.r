32d66087c23ca01983259832782f2884b565e853
Aurametrix <irene@aurametrix.com>
2016-10-03 15:40:21 +0400

    more examples
    .* is necessary as you need to match the entire string. Without that, nothing is removed. Also, sub can be used here instead of gsub

--- REGEX/number.r
+++ REGEX/number.r
@@ -9,3 +9,16 @@ str_extract("aaa12xxx", "[0-9]+")
 # Hadley Wickham's stringr package wraps all the existing regular expression operations
 # can also use strapply in the gsubfn package
 # or gsub("[^0-9]", "", "aaa12xxxx")
+
+# pattern is by finding a set of numbers in the start and capturing them
+as.numeric(gsub("([0-9]+).*$", "\\1", years))
+
+# an alternativewith a simpler Perl-like regular expression:
+as.numeric(gsub("[^\\d]+", "", years, perl=TRUE))
+
+# pattern is to just remove _years_old
+as.numeric(gsub(" years old", "", years))
+
+# split by space, get the element in first index
+as.numeric(sapply(strsplit(years, " "), "[[", 1))
+
