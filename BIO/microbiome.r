3588955a21b08b88621bc9f7b47f084bcccb03fb
Aurametrix <irene@aurametrix.com>
2018-04-12 17:22:13 -0700

    sample microbiome analyses
    https://bioconductor.org/packages/3.7/bioc/vignettes/microbiome/inst/doc/vignette.html

---
+++ BIO/microbiome.r
@@ -0,0 +1,13 @@
+library(BiocInstaller)
+source("http://www.bioconductor.org/biocLite.R")
+useDevel()
+biocLite("microbiome")
+
+# Data from 
+# http://www.nature.com/ncomms/2014/140708/ncomms5344/full/ncomms5344.html
+data(atlas1006) 
+atlas1006
+## phyloseq-class experiment-level object
+## otu_table()   OTU Table:         [ 130 taxa and 1172 samples ]
+## sample_data() Sample Data:       [ 1172 samples by 10 sample variables ]
+## tax_table()   Taxonomy Table:    [ 130 taxa by 2 taxonomic ranks ]
