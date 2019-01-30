c1faa172020c5d1ed04b910a06cf436224c017af
Aurametrix <irene@aurametrix.com>
2019-01-29 20:11:11 -0500

    ray shader
    https://github.com/tylermorganwall/rayshader
    https://www.rayshader.com/
    https://news.ycombinator.com/item?id=19017079

---
+++ VIZ/raymat.r
@@ -0,0 +1,30 @@
+library(rayshader)
+
+#Here, I load a map with the raster package.
+loadzip = tempfile() 
+download.file("https://tylermw.com/data/dem_01.tif.zip", loadzip)
+localtif = raster::raster(unzip(loadzip, "dem_01.tif"))
+unlink(loadzip)
+
+#And convert it to a matrix:
+elmat = matrix(raster::extract(localtif,raster::extent(localtif),buffer=1000),
+               nrow=ncol(localtif),ncol=nrow(localtif))
+
+#We use another one of rayshader's built-in textures:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  plot_map()
+  
+  #sphere_shade can shift the sun direction:
+elmat %>%
+  sphere_shade(sunangle = 45, texture = "desert") %>%
+  plot_map()
+
+raymat = ray_shade(elmat)
+
+#And we can add a raytraced layer from that sun direction as well:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color="desert") %>%
+  add_shadow(raymat) %>%
+  plot_map()
